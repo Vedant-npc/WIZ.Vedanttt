704053dfb4f2d88de257071422040ab19cd7289f
WIZ.Vedanttt ;) <mandevedant11@gmail.com>
2024-11-18 19:52:57 +0530

    Update star.c

--- star.c
+++ star.c
@@ -6,7 +6,10 @@ int main()
 
     printf("Enter Any Number: ");
     scanf("%d",&n);
-
+    //for 0--> 1 star
+   //for 1--> 3 star
+   //for 2--> 5 star
+   //for n--> 2*n+1 star
     for ( i = 0; i < n; i++)
     {
         for ( j = 0; j < 2*i+1; j++)
